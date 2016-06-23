2bf184f3579a5d27cfc2e4a9d2f4a6712833b3e2
Frederico Sales <fbiela@gmail.com>
2016-06-23 08:35:17 -0300

    add files to repository

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# import
+import jedi
+import gi
+
+# PyGObject
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository import GObject
+
+#==============================================================================#
+
+# class
+class Dummy():
+    # constructor
+    def __init__(self, *args):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file('interface.ui')
+        self.builder.connect_signal(self)
+
+    # destroy
+    def destroy(self, *args):
+        Gtk.main_quit()
+
+    # Run
+    def Run(self, *args):
+        self.builder.get_object('window1')
+        Gtk.main()
+
+#==============================================================================#
+if __name__ == "__main__":
+    user = Dummy()
+    user.Run()
